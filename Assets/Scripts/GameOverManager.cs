febc991f7e6c187d0c2bb76a4bbecb68f9341bdc
i <Douchebag@users.noreply.github.com>
2019-03-21 19:50:37 +0000

    isl comment

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// birtir game over screen thegar player deyr og reload-ar scene-inu
 public class GameOverManager : MonoBehaviour
 {
     public PlayerHealth playerHealth;
